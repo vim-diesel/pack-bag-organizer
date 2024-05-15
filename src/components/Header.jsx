7cef8b7813b3d0ddc9fbd3a176e36945b8fd9554
Ian C <ian.clark@hotmail.com>
2024-05-15 14:28:17 +0200

    styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,16 @@
-// import styled from "styled-components"
+import styled from 'styled-components';
 
 export default function Header() {
-  return (
-    <div>Header</div>
-  )
+  return <StyledHeader>Header</StyledHeader>;
 }
+
+const StyledHeader = styled.header`
+  grid-column: 1 / 3;
+  grid-row: 1 / 2;
+  background-color: #fbf5ed;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.05);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 28px;
+`;
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,7 +1,20 @@
-// import React from 'react'
+import styled from 'styled-components';
 
 export default function ItemList() {
   return (
-    <div>ItemList</div>
-  )
+    <StyledItemList>
+      <li>Item 1</li>
+      <li>Item 2</li>
+    </StyledItemList>
+  );
 }
+
+const StyledItemList = styled.ul`
+  grid-column: 1 / 2;
+  grid-row: 2 / 3;
+  background-color: #fff;
+  overflow-y: scroll;
+  scrollbar-color: red orange;
+  scrollbar-width: thin;
+  position: relative;
+`;
