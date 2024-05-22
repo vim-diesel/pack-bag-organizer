7c779b6ff8025bf792f1041133f6dc9f06aeba9f
Ian C <ian.clark@hotmail.com>
2024-05-22 15:54:12 +0200

    style our general button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,20 @@
-import styled from "styled-components"
+import styled from 'styled-components';
 
-export default function Button({children}) {
-  return (
-    <StyledButton>{children}</StyledButton>
-  )
+export default function Button({ children }) {
+  return <StyledButton>{children}</StyledButton>;
 }
 
-const StyledButton = styled.button``;
+const StyledButton = styled.button`
+  height: 45px;
+  width: 100%;
+  border: none;
+  border-radius: 5px;
+  background-color: #473a2b;
+  color: #fff;
+  font-size: 16px;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: all 0.2s;
+`;
