1fcaa392c6a61d461f52d8c3f0fdb3905c3de608
Ian C <ian.clark@hotmail.com>
2024-05-22 15:59:09 +0200

    style ManageItemButtons component

--- src/components/ManageItemButtons.jsx
+++ src/components/ManageItemButtons.jsx
@@ -1,10 +1,17 @@
-// import React from 'react'
+import styled from 'styled-components';
 
 export default function ManageItemButtons() {
   return (
-    <>
-      <button type="button">Pack</button>
-      <button type="button">Remove</button>
-    </>
-  )
+    <StyledButtonGroup>
+      <button type='button'>Pack</button>
+      <button type='button'>Remove</button>
+    </StyledButtonGroup>
+  );
 }
+
+const StyledButtonGroup = styled.section`
+  margin-top: auto;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
