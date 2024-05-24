46fc5e469f920312502b4419dd11ef50e19b15ae
Ian C <ian.clark@hotmail.com>
2024-05-24 13:30:18 +0200

    refactor: Update EmptyView component to use styled-components for styling

--- src/components/EmptyView.jsx
+++ src/components/EmptyView.jsx
@@ -2,12 +2,21 @@ import styled from 'styled-components';
 
 export default function EmptyView() {
   return (
-    <section>
+    <StyledSection>
       <StyledHeader>Empty Packing List</StyledHeader>
       <StyledParagraph>Nothing to pack yet!</StyledParagraph>
-    </section>
+    </StyledSection>
   );
 }
+
+const StyledSection = styled.section`
+  grid-column: 1 / 2;
+  grid-row: 2 / 3;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 const StyledHeader = styled.h3`
   font-size: 1.5rem;
